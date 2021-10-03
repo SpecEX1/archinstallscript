fb28cab075e690f46c05152954aa14948e9b5d64
SpecEX1 <89618616+SpecEX1@users.noreply.github.com>
2021-10-03 18:05:53 -0400

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -5,8 +5,23 @@ mount /dev/vda3 /mnt
 mkdir /mnt/boot
 mount /dev/vda1 /mnt/boot
 swapon /dev/vda2
-pacstrap /mnt base base-devel linux linux-firmware neovim git
+pacstrap /mnt base base-devel linux linux-firmware nano
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/America/Detroit /etc/localtime
 hwclock --systohc
+# Uncomment en_US.UTF-8 UTF-8
+nano /etc/locale.gen
+locale-gen
+# LANG=en_US.UTF-8
+nano /etc/locale.conf
+# myhostname
+nano /etc/hostname
+mkinitcpio -P
+passwd
+pacman -Sy grub efibootmgr
+# Add --removable after --efi-directory=esp if installing on a removable drive or have an MSI motherboard
+grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+reboot
